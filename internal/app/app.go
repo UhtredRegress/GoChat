5b4d6cc26b0df7b0821530ac0a0dc4a43cf75211
Kevin Do <dominhtriet47@gmail.com>
2026-03-24 17:03:38 +0700

    Init user handler to handle request about user

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,9 +1,16 @@
 package app
 
-type App struct {
+import "github.com/gin-gonic/gin"
 
+type App struct {
+	router *gin.Engine
 }
 
 func NewApp() *App {
-	return &App{}
+	router := gin.Default()
+	return &App{router}
+}
+
+func (a *App) Run() {
+
 }
